c80e86741c71e36b8714180149af4b445f0f0eac
Luiza Campos <luiza.almcampos@gmail.com>
2021-01-26 10:56:35 -0300

    segundo dia - url gitHub att

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/luizaacampos" />
     </QuizBackground>
   );
 }
